39ad6e91b48c911324b270d763fbdee7fdaf0b3a
moxy37 <dbrennan@nu-worx.com>
2020-02-04 18:58:54 +0000

    ds

--- test.sql
+++ test.sql
@@ -1,1 +1,1 @@
- SELECT guid FROM wp_posts WHERE ID IN (SELECT CASE WHEN post_parent=0 THEN ID ELSE post_parent END FROM wp_posts WHERE post_content LIKE '%embedded-vision%');
+ SELECT guid FROM wp_posts WHERE ID IN (SELECT CASE WHEN post_parent=0 THEN ID ELSE post_parent END FROM wp_posts WHERE post_content LIKE '%embedded-vision%') AND post_status='publish';
