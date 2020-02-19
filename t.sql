bbe0078ba217f3383140cef85030f91b0d18ad90
moxy37 <dbrennan@nu-worx.com>
2020-02-19 18:40:24 +0000

    WTF

--- t.sql
+++ t.sql
@@ -1,112 +1,109 @@
-UPDATE wp_posts SET guid=REPLACE(guid, '159.203.10.180', '159.203.3.222');
-UPDATE wp_posts SET post_content=REPLACE(post_content, '159.203.10.180', '159.203.3.222');
-UPDATE wp_postmeta SET meta_value=REPLACE(meta_value, '159.203.10.180', '159.203.3.222');
-
-
-SELECT meta_key, COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%159.203.3.222%' GROUP BY meta_key;
-
-'%http:\/\/159.203.3.222%'
-
-
-INSERT INTO wp_nextend2_smartslider3_slides
-INSERT INTO `wp_toolset_post_guid_id` VALUES ('http://159.203.3.222/wp-content/uploads/2019/12/picture-9.jpg',11);
-INSERT INTO `wp_usermeta` VALUES (307,3,'wpcf-picture','http://159.203.3.222/wp-content/uploads/2019/12/picture-4.jpg');
-
-SELECT ID FROM wp_posts WHERE ID IN (SELECT post_id FROM wp_postmeta WHERE meta_value LIKE '%159.203.3.222%' AND meta_value NOT LIKE '%http://159.203.3.222%') AND post_parent=0;
-
-SELECT COUNT(*) FROM wp_toolset_post_guid_id WHERE guid LIKE '%http://159.203.3.222%';
-SELECT COUNT(*) FROM wp_usermeta WHERE meta_value LIKE '%http://159.203.3.222%';
-SELECT COUNT(*) FROM wp_nextend2_smartslider3_slides WHERE slide LIKE '%http://159.203.3.222%';
-
-SELECT COUNT(*) FROM wp_nextend2_smartslider3_slides WHERE slide LIKE '%http:\/\/159.203.3.222%';
-SELECT COUNT(*) FROM wp_posts WHERE post_content LIKE '%http://159.203.3.222%';
-SELECT COUNT(*) FROM wp_posts WHERE guid LIKE '%http://159.203.3.222%';
-SELECT COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%http://159.203.3.222%';
-SELECT meta_key, COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%159.203.3.222%' GROUP BY meta_key;
-SELECT meta_key, COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%http:\/\/159.203.3.222%' GROUP BY meta_key;
-
-SELECT meta_key, COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%http://159.203.3.222%' GROUP BY meta_key;
-
-SELECT COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%http:\/\/159.203.3.222%' AND meta_key='_elementor_data';
-SELECT COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%http:\\159.203.3.222%' AND meta_key='_elementor_data';
-SELECT meta_id FROM wp_postmeta WHERE meta_value LIKE '%159.203.3.222%' AND meta_key='_elementor_data' AND meta_value LIKE 'http:\/\/159.203.3.222';
- --AND meta_value NOT LIKE '%http:\/\/159.203.3.222%' GROUP BY meta_key;
-SELECT meta_key, COUNT(*) FROM wp_postmeta WHERE meta_value LIKE  '%http:\/\/159.203.3.222%' GROUP BY meta_key;
-SELECT meta_key, COUNT(*) FROM wp_postmeta WHERE meta_value LIKE  '%http:\\/\\/159.203.3.222%' GROUP BY meta_key;
-
-SELECT * FROM wp_postmeta WHERE meta_value LIKE  '%http:\\/\\/159.203.3.222%'  AND meta_key='_elementor_data'\G
-
- AND meta_value NOT LIKE '%http:\/\/159.203.3.222%' AND meta_value NOT LIKE '%http:\\/\\/159.203.3.222%';
-
-SELECT meta_id FROM wp_postmeta WHERE meta_value LIKE '%http:\/\/159.203.3.222%' AND meta_key='_elementor_data';
-SELECT meta_id FROM wp_postmeta WHERE meta_value LIKE '%http:\\159.203.3.222%' AND meta_key='_elementor_data';
-SELECT * FROM wp_postmeta WHERE meta_value LIKE '%159.203.3.222%' AND meta_key='_elementor_data' AND meta_value NOT LIKE '%http:\/\/159.203.3.222%' AND meta_value NOT LIKE '%http:\\/\\/159.203.3.222%';
-
-
-
-
-SELECT COUNT(*) FROM wp_postmeta WHERE meta_value NOT LIKE '%http%' AND meta_value LIKE '%159.203.3.222%' AND meta_key='_elementor_data';
-
-UPDATE wp_postmeta SET meta_value=REPLACE(meta_value, '159.203.3.222', 'www.edge-ai-vision.com') WHERE meta_key='_elementor_data';
-UPDATE wp_postmeta SET meta_value=REPLACE(meta_value, 'http://159.203.3.222', 'http://www.edge-ai-vision.com') WHERE meta_key<>'_elementor_data';
-UPDATE wp_toolset_post_guid_id SET guid=REPLACE(guid, 'http://159.203.3.222', 'http://www.edge-ai-vision.com');
-UPDATE wp_posts SET guid=REPLACE(guid, 'http://159.203.3.222', 'http://www.edge-ai-vision.com');
-UPDATE wp_posts SET post_content=REPLACE(post_content, 'http://159.203.3.222', 'http://www.edge-ai-vision.com');
-UPDATE wp_nextend2_smartslider3_slides SET slide=REPLACE(slide, 'http://159.203.3.222', 'http://www.edge-ai-vision.com');
-UPDATE wp_usermeta SET meta_value=REPLACE(meta_value, 'http://159.203.3.222', 'http://www.edge-ai-vision.com');
-UPDATE wp_toolset_post_guid_id SET guid=REPLACE(guid, 'http://159.203.3.222', 'http://www.edge-ai-vision.com');
-
-SELECT meta_key, COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%http:\/\/159.203.3.222%' GROUP BY meta_key;
-
-SELECT * FROM wp_postmeta WHERE meta_value LIKE '%159.203.3.222%' AND meta_value NOT LIKE '%http://159.203.3.222%' AND post_id=11038\G
-
-SELECT COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%159.203.3.222%' AND meta_key='_elementor_data' AND meta_value NOT LIKE '%http%';
-SELECT COUNT(*) FROM wp_options WHERE option_value LIKE '%159.203.3.222%';
-SELECT COUNT(*) FROM wp_options WHERE option_value LIKE '%http://159.203.3.222%';
-
-http:\/\/www.edge-ai-vision.com
-SELECT COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%http:\/\/www.edge-ai-vision.com%' AND meta_key='_elementor_data';
-SELECT COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%http://www.edge-ai-vision.com%' AND meta_key='_elementor_data';
-SELECT COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%www.edge-ai-vision.com%' AND meta_key='_elementor_data' AND meta_value NOT LIKE '%http://www.edge-ai-vision.com%' AND meta_key='_elementor_data' AND meta_value NOT LIKE '%http:\/\/www.edge-ai-vision.com%' AND meta_key='_elementor_data';
-http:\/\/www.edge-ai-vision.com
-
-SELECT COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%clone.embedded-vision.com%'   AND meta_key='_elementor_data' ;
-GROUP BY meta_key;
-CREATE TABLE monkey (
-    post_id BIGINT(20) unsigned,
-    post_parent BIGINT(20) unsigned
-);
-DELETE FROM monkey;
-INSERT INTO monkey (post_id, post_parent) SELECT ID, post_parent FROM wp_posts WHERE ID IN (SELECT post_id FROM wp_postmeta WHERE meta_value LIKE '%www.edge-ai-vision.com%' AND meta_key='_elementor_data') AND meta_value NOT LIKE '%http://www.edge-ai-vision.com%';
-SELECT post_parent, COUNT(*) FROM monkey WHERE post_parent<>0 GROUP BY post_parent;
-
-AND post_parent=0;
-;
-
-UPDATE wp_options SET option_value=REPLACE(option_value, 'http://159.203.3.222', 'http://www.edge-ai-vision.com');
-UPDATE wp_postmeta SET meta_value=REPLACE(meta_value, '159.203.3.222', 'www.edge-ai-vision.com') WHERE meta_key='_elementor_data';
-UPDATE wp_postmeta SET meta_value=REPLACE(meta_value, 'http://159.203.3.222', 'http://www.edge-ai-vision.com') WHERE meta_key<>'_elementor_data';
---Trying this one
-ALTER TABLE wp_toolset_post_guid_id MODIFY guid VARCHAR(255);
-UPDATE wp_toolset_post_guid_id SET guid=REPLACE(guid, 'http://159.203.3.222', 'http://www.edge-ai-vision.com');
-ALTER TABLE wp_toolset_post_guid_id MODIFY guid VARCHAR(190);
-
-UPDATE wp_posts SET guid=REPLACE(guid, 'http://159.203.3.222', 'http://www.edge-ai-vision.com');
-UPDATE wp_posts SET post_content=REPLACE(post_content, 'http://159.203.3.222', 'http://www.edge-ai-vision.com');
-UPDATE wp_nextend2_smartslider3_slides SET slide=REPLACE(slide, 'http://159.203.3.222', 'http://www.edge-ai-vision.com');
-UPDATE wp_usermeta SET meta_value=REPLACE(meta_value, 'http://159.203.3.222', 'http://www.edge-ai-vision.com');
-
-
-
---Next do these
-SELECT * FROM wp_postmeta WHERE meta_value LIKE '%www.edge-ai-vision.com%' AND meta_key='_elementor_data'\G
-SELECT COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%http:\/\/www.edge-ai-vision.com%' AND meta_key='_elementor_data';
-SELECT COUNT(*) FROM wp_postmeta WHERE meta_value LIKE '%http:\\/\\/www.edge-ai-vision.com%' AND meta_key='_elementor_data';
-UPDATE wp_postmeta SET meta_value=REPLACE(meta_value, 'http://www.edge-ai-vision.com', 'https://www.edge-ai-vision.com') WHERE meta_key<>'_elementor_data';
-UPDATE wp_toolset_post_guid_id SET guid=REPLACE(guid, 'http://www.edge-ai-vision.com', 'https://www.edge-ai-vision.com');
-UPDATE wp_options SET option_value=REPLACE(option_value, 'http://www.edge-ai-vision.com', 'https://www.edge-ai-vision.com');
-UPDATE wp_posts SET guid=REPLACE(guid, 'http://www.edge-ai-vision.com', 'https://www.edge-ai-vision.com');
-UPDATE wp_posts SET post_content=REPLACE(post_content, 'http://www.edge-ai-vision.com', 'https://www.edge-ai-vision.com');
-UPDATE wp_nextend2_smartslider3_slides SET slide=REPLACE(slide, 'http://www.edge-ai-vision.com', 'https://www.edge-ai-vision.com');
-UPDATE wp_usermeta SET meta_value=REPLACE(meta_value, 'http://www.edge-ai-vision.com', 'https://www.edge-ai-vision.com');
-
+SELECT * FROM wp_eva_db.wp_commentmeta WHERE meta_key LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_commentmeta WHERE meta_value LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_comments WHERE comment_author LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_comments WHERE comment_author_email LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_comments WHERE comment_author_url LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_comments WHERE comment_author_IP LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_comments WHERE comment_content LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_comments WHERE comment_approved LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_comments WHERE comment_agent LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_comments WHERE comment_type LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_fg_redirect WHERE old_url LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_fg_redirect WHERE type LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_links WHERE link_url LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_links WHERE link_name LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_links WHERE link_image LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_links WHERE link_target LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_links WHERE link_description LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_links WHERE link_visible LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_links WHERE link_rel LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_links WHERE link_notes LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_links WHERE link_rss LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_image_storage WHERE hash LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_image_storage WHERE image LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_image_storage WHERE value LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_section_storage WHERE application LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_section_storage WHERE section LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_section_storage WHERE referencekey LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_section_storage WHERE value LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_generators WHERE group LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_generators WHERE type LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_generators WHERE params LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_sliders WHERE alias LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_sliders WHERE title LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_sliders WHERE type LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_sliders WHERE params LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_sliders WHERE thumbnail LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_slides WHERE title LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_slides WHERE slide LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_slides WHERE description LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_slides WHERE thumbnail LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_nextend2_smartslider3_slides WHERE params LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_options WHERE option_name LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_options WHERE option_value LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_options WHERE autoload LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_pmxe_exports WHERE options LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_pmxe_exports WHERE scheduled LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_pmxe_exports WHERE friendly_name LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_pmxe_exports WHERE export_post_type LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_pmxe_google_cats WHERE name LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_pmxe_google_cats WHERE parent_name LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_pmxe_templates WHERE name LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_pmxe_templates WHERE options LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_postmeta WHERE meta_key LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_postmeta WHERE meta_value LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE post_content LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE post_title LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE post_excerpt LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE post_status LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE comment_status LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE ping_status LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE post_password LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE post_name LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE to_ping LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE pinged LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE post_content_filtered LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE guid LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE post_type LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_posts WHERE post_mime_type LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_signups WHERE domain LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_signups WHERE path LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_signups WHERE title LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_signups WHERE user_login LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_signups WHERE user_email LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_signups WHERE activation_key LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_signups WHERE meta LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_term_taxonomy WHERE taxonomy LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_term_taxonomy WHERE description LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_termmeta WHERE meta_key LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_termmeta WHERE meta_value LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_terms WHERE name LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_terms WHERE slug LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_post_guid_id WHERE guid LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE slug LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE display_name_plural LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE display_name_singular LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE driver LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE parent_domain LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE child_domain LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE intermediary_type LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE ownership LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE scope LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE origin LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE role_name_parent LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE role_name_child LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE role_name_intermediary LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE role_label_parent_singular LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE role_label_child_singular LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE role_label_parent_plural LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_relationships WHERE role_label_child_plural LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_toolset_type_sets WHERE type LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_usermeta WHERE meta_key LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_usermeta WHERE meta_value LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_users WHERE user_login LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_users WHERE user_pass LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_users WHERE user_nicename LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_users WHERE user_email LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_users WHERE user_url LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_users WHERE user_activation_key LIKE '%www.edge-ai-vision.com%';
+SELECT * FROM wp_eva_db.wp_users WHERE display_name LIKE '%www.edge-ai-vision.com%';
